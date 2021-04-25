a2e33fb240e004c2d46ad1075a29a54f3fbc4721
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-26 02:31:14 +0530

    ADDED: Ability to add a new contact to addresss book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class contactCategories {
+class ContactCategories {
     Scanner input = new Scanner(System.in);
     //can store multiple contact details
     ArrayList<String> firstName = new ArrayList<String>();
@@ -16,56 +16,72 @@ class contactCategories {
 
     //creating methods for various sub sections of the contact
     public String first_Name() {
-        System.out.println("Enter the first name: ");
+        System.out.print("Enter the first name: ");
         String firstNameInput = input.nextLine();
         return firstNameInput;
     }
 
     public String last_Name() {
-        System.out.println("Enter the last name: ");
+        System.out.print("Enter the last name: ");
         String lastNameInput = input.nextLine();
         return lastNameInput;
     }
 
     public String address_() {
-        System.out.println("Enter the address");
+        System.out.print("Enter the address");
         String addressInput = input.nextLine();
         return addressInput;
     }
 
     public String city_Name() {
-        System.out.println("Enter the name of the city: ");
+        System.out.print("Enter the name of the city: ");
         String cityInput = input.nextLine();
         return cityInput;
     }
 
     public String state_Name() {
-        System.out.println("Enter the name of the state: ");
+        System.out.print("Enter the name of the state: ");
         String stateInput = input.nextLine();
         return stateInput;
     }
 
     public int zip_Code() {
-        System.out.println("Enter the zip code: ");
+        System.out.print("Enter the zip code: ");
         int zipInput = input.nextInt();
         return zipInput;
     }
 
     public int phone_Number() {
-        System.out.println("Enter the phone number: ");
+        System.out.print("Enter the phone number: ");
         int phoneNumberInput = input.nextInt();
         return phoneNumberInput;
     }
 
     public String email_Id() {
-        System.out.println("Enter the email id: ");
+        System.out.print("Enter the email id: ");
         String emailIdInput = input.nextLine();
         return emailIdInput;
+    }
+
+    //Ability to add new contact to Address Book
+    public void addPersonContactDetail(){
+        System.out.println("Add a new person in the Address Book");
+        firstName.add(first_Name());
+        lastName.add(last_Name());
+        address.add(address_());
+        city.add(city_Name());
+        state.add(state_Name());
+        zipCode.add(zip_Code());
+        phoneNumber.add(phone_Number());
+        emailId.add(email_Id());
     }
 }
 
 public class AddressBook {
     public static void main(String args[]) {
         System.out.println("Welcome to the Address Book !!");
+        ContactCategories contactCategories = new ContactCategories();
+        contactCategories.addPersonContactDetail();
     }
+
 }
